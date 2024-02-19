0bc82494bfbf9b6a61367fb3da069f270408a867
azizul016 <haqueazizul789@gmail.com>
2024-02-19 23:21:37 +0600

    added debounce & without debounce withcomments

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -2,29 +2,50 @@ import { useContext, useState } from "react";
 import searchIcon from "./../../assets/search.svg";
 import { getLocationByName } from "../../data/location-data";
 import { LocationContext } from "../../context";
+import { useDebounce } from "../../hooks";
 
 export default function Search() {
-  const [searchTerm, setSearchTerm] = useState();
+  //use without debounce search
+  // const [searchTerm, setSearchTerm] = useState();
   const { setSelectedLocation } = useContext(LocationContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // console.log(searchTerm, "searchTerm");
-    const feachedLocation = getLocationByName(searchTerm);
-    // console.log(feachedLocation, "feachedLoaction");
-    setSelectedLocation({ ...feachedLocation });
+  //use without debounce search
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // console.log(searchTerm, "searchTerm");
+  //   const feachedLocation = getLocationByName(searchTerm);
+  //   // console.log(feachedLocation, "feachedLoaction");
+  //   setSelectedLocation({ ...feachedLocation });
+  // };
+
+  //for de bounce search
+  const doSearch = useDebounce((term) => {
+    const feachedLoaction = getLocationByName(term);
+    setSelectedLocation({ ...feachedLoaction });
+  }, 500);
+
+  //for de bounce search
+  const handleChange = (e) => {
+    const value = e.target.value;
+    doSearch(value);
   };
   return (
     <>
-      <form action="#" onSubmit={handleSubmit}>
+      <form
+        action="#"
+        // use without debounce search
+        // onSubmit={handleSubmit}
+      >
         <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
           <input
             className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
             type="search"
             placeholder="Search Location"
             required
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            // value={searchTerm}
+            onChange={handleChange}
+            // use without debounce search
+            // onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button type="submit">
             <img src={searchIcon} alt="Search" />
